f622bd6cb625da6ad7d5a75cbcd8fa06956d4415
creatosaurus <creatosaurus.in@gmail.com>
2020-03-21 11:25:53 +0530

    make it better

--- Routes/User.js
+++ Routes/User.js
@@ -181,7 +181,7 @@ router.get('/accept/:token', (req, res, next) => {
         team.find({ creator_email: decoded.sender_email, peoples: decoded.user_email }).then(data => {
           if (data.length === 0) {
             team.updateOne({ creator_email: decoded.sender_email }, { $push: { peoples: decoded.user_email } }).then(data => {
-              res.redirect(`http://192.168.43.29:3000/login`)
+              res.redirect(`http://192.168.43.29:3000/`)
             }).catch(err => {
               return res.status(500).json({ error: err })
             })
